3e24d64506c465c908df486161e860f6dbaeabbe
Ashley Dhevalall <ashleysdhevalall@yahoo.com>
2025-04-02 11:06:26 +0200

    clean up

--- my-first-script/main.py
+++
@@ -1,8 +0,0 @@
-import ollama
-
-model = "deepseek-r1:7b"
-
-while True:
-    Myprompt = input("You: ")
-    answer = ollama.generate(model=model, prompt=Myprompt)
-    print("DeepSeek:", answer["response"])
--- my-first-script/myenv/Scripts/main.py
+++
@@ -1,8 +0,0 @@
-import ollama
-
-model = "deepseek-r1:latest"
-
-while True:
-    Myprompt = input("You: ")
-    answer = ollama.generate(model=model, prompt=Myprompt)
-    print("DeepSeek:", answer["response"])
